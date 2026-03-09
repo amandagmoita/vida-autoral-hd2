ecadfa472fad7b2d744dff5ac40dcfdab2308f7c
amandagmoita <amandagmoita@gmail.com>
2026-03-09 17:41:26 -0300

    submit.js

--- api/submit.js
+++ api/submit.js
@@ -250,8 +250,8 @@ if (hd.SVG) {
     // Escala fit-contain: caber na area disponivel
     const scaleX = CHART_W / vbW;
     const scaleY = AREA_H / vbH;
-    // 0.96 = margem minima para numeros de portoes fora do viewBox
-    const scale = Math.min(scaleX, scaleY) * 0.96;
+    // 1.4 = margem minima para numeros de portoes fora do viewBox
+    const scale = Math.min(scaleX, scaleY) * 1.4;
     const fitW = vbW * scale;
     const fitH = vbH * scale;
 
